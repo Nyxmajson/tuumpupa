57f2654011e253a35f6f5615f3aae94b40b27b75
Jason Nago <jason.nago@mmibordeaux.com>
2022-04-01 09:12:19 +0200

    Modification de la page création

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,6 @@
 Rails.application.routes.draw do
-  get 'creation/character'
-
   root 'home#index'
+  get 'creation/character'
   get 'home/index'
   resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
